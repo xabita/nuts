c017a7fd57939c8f13ea3c9b2655a74bd84371ef
xabita <mutskxabaa@gmail.com>
2018-01-31 01:40:55 -0600

    Comments
    Save comments and show  comments list

--- apps/faqs/urls.py
+++ apps/faqs/urls.py
@@ -4,5 +4,6 @@ from apps.faqs import views as faqs_views
 
 
 urlpatterns = [
-    url(r'^comments/add/$', faqs_views.add_comments, name='add_comments'),
+
+    url(r'^comments/add/(?P<pk>[0-9]+)/$', faqs_views.add_comments, name='add_comments'),
 ]
--- apps/faqs/views.py
+++ apps/faqs/views.py
@@ -5,12 +5,12 @@ from apps.courses.models import Resource
 from apps.faqs.forms import ResourceCommentForm
 
 # Create your views here.
-def add_comments(request):
+def add_comments(request, pk):
 	if request.method == "POST":
 		form = ResourceCommentForm(request.POST)
 		if form.is_valid():
 			comment = form.save(commit=False)
-			resource_tmp = Resource.objects.get(pk=request.POST.get("resource",))
+			resource_tmp = Resource.objects.get(pk=pk)
 			comment.resource = resource_tmp
 			comment.save()
 			return redirect('resource_detail', resource_tmp.id)
